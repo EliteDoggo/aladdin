5637aeab2551ca7332cd266b6d6e631bfeebe8a7
EliteDoggo <karkuzaev.soslan19991@gmail.com>
2019-10-30 15:12:24 +0300

    a

--- game/js/ger.js
+++ game/js/ger.js
@@ -8,8 +8,13 @@ let $start = document.querySelector('#start'),
 
 
 
-let G = 1;
-let bananas,
+const G = 1;
+let images = {},
+		width,
+		height,
+		canvas,
+		ctx,
+		bananas,
 		hp,
 		startTime,
 		name,
@@ -17,21 +22,55 @@ let bananas,
 
 
 
+load();
 
-init();
+
+async function load() {
+	images.bg = await loadImage('media/bg/bg.png');
+	images.ground = await loadImage('media/clipart/194900.png');
+	// images.aladdin = await loadImage('...');
+	// images.aladdinRun = [];
+	// images.aladdinRun[0] = await loadImage('aladdinRun1.png');
+	// images.aladdinRun[1] = await loadImage('aladdinRun2.png');
+	// images.aladdinRun[2] = await loadImage('aladdinRun3.png');
+	// images.aladdinRun[3] = await loadImage('aladdinRun4.png');
+	// images.aladdinRun[4] = await loadImage('aladdinRun5.png');
+
+	init();
+}
+
+
+
+function loadImage(path) {
+	return new Promise(res => {
+		let img = new Image();
+		img.onload = res(img);
+		img.src = path;
+	});
+}
+
+
+
+
 
 
 function init() {
 	bananas = 0;
-	hp = 5;
+	hp = 100;
 	startTime = new Date().getTime();
 	name = 'asd';
 	$tableName.innerText = name;
 
+	canvas = document.createElement('canvas');
+	width = canvas.width = window.innerWidth;
+	height = canvas.height = window.innerHeight;
+	ctx = canvas.getContext('2d');
+	document.body.appendChild(canvas);
+
 
 	updateTimer();
 	interval = setInterval(() => {
-		hp--;
+		// hp--;
 		updateTimer();
 		// addSnake();
 		
@@ -62,7 +101,8 @@ function update() {
 
 
 function draw() {
-	
+	ctx.drawImage(images.bg, 0, 0, width, images.bg.height,
+								0, 0, width, height);
 }
 
 
@@ -79,3 +119,8 @@ function die() {
 	$game.style.display = 'none';
 	$end.style.display = 'block';
 }
+
+
+
+
+
--- game/js/main.js
+++ game/js/main.js
@@ -1,9 +1,3 @@
-let canv = document.createElement('canvas');
-let ctx = canv.getContext('2d');
-canv.width = window.innerWidth;
-canv.height = window.innerHeight;
-document.body.appendChild(canv);
-
 let $start = document.querySelector('#start'),
     $game = document.querySelector('#game'),
     $end = document.querySelector('#end'),
@@ -12,15 +6,40 @@ let $start = document.querySelector('#start'),
     $tableBananas = document.querySelector('#tableBananas'),
     $tableTime = document.querySelector('#tableTime');
 
-
-let name,
+let G = 1;
+let canv,
+    ctx,
+    name,
     hp,
     bananas,
     time,
     interval,
-    startTime;
+    startTime,
+    images = {},
+    width,
+    height;
 
-init();
+
+
+load();
+
+async function load() {
+    
+    images.bg = await loadImage('../media/bg/bg.png');
+    images.ground = await loadImage('../media/clipart/194900.png');
+
+    init();
+}
+
+function loadImage(path){
+    return new Promise(res => {
+        let img = new Image();
+        img.onload = res(img);
+        img.src = path;
+    });
+}
+
+
 
 function init() {
     name = '';
@@ -28,6 +47,12 @@ function init() {
     bananas = 0;
     startTime = new Date().getTime();
     $tableName.innerText = name;
+
+    canv = document.createElement('canvas');
+    ctx = canv.getContext('2d');
+    width = canv.width = window.innerWidth;
+    height = canv.height = window.innerHeight;
+    document.body.appendChild(canv);
 
     updateTimer();
     interval = setInterval(() => {
@@ -55,6 +80,10 @@ function update() {
 }
 
 function draw() {
+    ctx.drawImage(images.bg, 0, 0, width, images.bg.height,
+                                0, 0, width, height);
+
+    ctx.drawImage(images.ground, 50, height - 100, 100, 70);
 
 }
 
@@ -68,3 +97,4 @@ function die() {
     $game.style.display = 'none';
     $end.style.display = 'block';
 }
+
